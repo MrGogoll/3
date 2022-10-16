931cba33b6b48c834530478e8445ed866a7f6746
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-10-16 17:06:31 +0300

    Add files via upload

---
+++ ggf1.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	
+	int a, b;
+	a = n / 1000;
+	b = n % 1000;
+	int s1, s2;
+	s1 = a % 10 + (a / 10) % 10 + a / 100;
+	s2 = b % 10 + (b / 10) % 10 + b / 100;
+
+	if ( s1 == s2 ) {
+		cout << "Yes";
+	}
+	else { cout << "No"; }
+
+}
